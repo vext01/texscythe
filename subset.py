cdbdc1d69d1616885a65490b7582ec30362f548e
Edd Barrett <vext01@gmail.com>
2013-08-21 10:08:38 +0100

    Woops! Add subsetter.

---
+++ subset.py
@@ -0,0 +1,54 @@
+# Copyright (c) 2013, Edd Barrett <edd@openbsd.org> <vext01@gmail.com>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import config
+from orm import Package, Dependency, File
+
+def compute_subset(include_pkgs, exclude_pkgs, outfilename="out.plist"):
+    # argparse gives None if switch is absent
+    if include_pkgs is None: include_pkgs = []
+    if exclude_pkgs is None: exclude_pkgs = []
+
+    # Set up ORM
+    engine = create_engine('sqlite:///%s' % (config.SQLDBPATH))
+    Session = sessionmaker(bind=engine)
+    sess = Session()
+
+    include_files = build_file_list(sess, include_pkgs)
+    exclude_files = build_file_list(sess, exclude_pkgs)
+    # XXX do subtract
+    # XXX write away
+
+def build_file_list(sess, packages):
+    # we have to be careful how we do this to not explode the memory.
+    # let's iteratively collect file lists from packages and accumulate them
+    # in a set. This will remove duplicates as we go.
+    files = set()
+    for p in packages:
+        files = files | build_file_list_pkg(sess, p)
+
+    return files
+
+def build_file_list_pkg(sess, pkgname):
+    print("Build file list: %s" % pkgname)
+    # XXX look up package
+    # add files
+    # process deps and union with the above files.
+    # return them
+    qry = sess.query(Package.pkgname).filter(Package.pkgname == pkgname).one()
+    print(qry)
+    return set() # XXX
