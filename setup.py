ec45d26aa00197d678ea456795f277fa9eb0df62
Edd Barrett <vext01@gmail.com>
2013-09-19 00:03:32 +0100

    Whitespace.

--- setup.py
+++ setup.py
@@ -3,15 +3,14 @@
 from distutils.core import setup
 import sys
 
-setup (name              = 'texscythe',
-       version           = '0.1',
-       description       = 'TeX Live texmf subsetter',
-       maintainer        = 'Edd Barrett',
-       maintainer_email  = 'vext01@gmail.com',
-       license           = 'ISC',
-       long_description  = 'TeX Live texmf subsetter',
-       url               = 'https://github.com/vext01/texscythe',
-       packages          = ['texscythe'],
-       package_dir       = {'': './lib/'},
-       )
-
+setup(name              = 'texscythe',
+      version           = '0.1',
+      description       = 'TeX Live texmf subsetter',
+      maintainer        = 'Edd Barrett',
+      maintainer_email  = 'vext01@gmail.com',
+      license           = 'ISC',
+      long_description  = 'TeX Live texmf subsetter',
+      url               = 'https://github.com/vext01/texscythe',
+      packages          = ['texscythe'],
+      package_dir       = {'': './lib/'},
+      )
