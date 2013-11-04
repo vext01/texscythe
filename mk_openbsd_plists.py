c30433663d3f94318bc39ed58fbe90759319fe3b
Edd Barrett <vext01@gmail.com>
2013-11-04 01:18:07 +0000

    Use global regex filter to exclude pdf manuals and single quote all regexes.

--- mk_openbsd_plists.py
+++ mk_openbsd_plists.py
@@ -12,19 +12,24 @@ class NastyError(Exception): pass
 # This is how we detect manual pages
 MAN_REGEX="texmf-dist\/doc\/man\/man1\/.*[0-9]$"
 
-def run_texscyther(inc_specs, exc_specs, plist):
+def run_texscyther(inc_specs, exc_specs, plist, global_re=""):
     if inc_specs:
+        inc_specs = [ "'%s'" % x for x in inc_specs ]
         inc_specs = "-i " + " ".join(inc_specs)
     else:
         inc_specs = ""
 
     if exc_specs:
+        exc_specs = [ "'%s'" % x for x in inc_specs ]
         exc_specs = "-x " + " ".join(exc_specs)
     else:
         exc_specs = ""
 
-    cmd = "%s texscyther --subset %s %s -o %s" % (
-        sys.executable, inc_specs, exc_specs, plist
+    if global_re != "":
+        global_re = "-r '%s'" % global_re
+
+    cmd = "%s texscyther --subset %s %s -o %s %s" % (
+        sys.executable, inc_specs, exc_specs, plist, global_re
         )
     print ("Running: %s" % cmd)
     if os.system(cmd) != 0:
@@ -85,10 +90,24 @@ minimal_specs = runs_and_mans(["scheme-tetex"])
 run_texscyther(minimal_specs, buildset_specs, "PLIST-minimal")
 
 
+# /----------------------------------------------------------
+# | FULL
+# +----------------------------------------------------------
+# | Everything bar docs (other than relevant manuals)
+# \----------------------------------------------------------
+
 print(">>> texlive_texmf-full")
 full_specs = runs_and_mans(["scheme-full"])
 run_texscyther(full_specs, minimal_specs, "PLIST-full")
 
-# XXX exclude dumb pdf manuals
+# /----------------------------------------------------------
+# | FULL
+# +----------------------------------------------------------
+# | All remaining docs
+# \----------------------------------------------------------
+
+# exclude manuals and dumb pdf manuals
+MAN_PDFMAN_REGEX="(?!texmf-dist\/doc\/man\/man[0-9]\/(.*[0-9]|.*.man[0-9].pdf)$)"
+
 print(">>> texlive_texmf-docs")
-run_texscyther(["scheme-full:doc"], ["scheme-full:doc:%s" % MAN_REGEX], "PLIST-doc")
+run_texscyther(["scheme-full:doc"], [], "PLIST-doc", MAN_PDFMAN_REGEX)
