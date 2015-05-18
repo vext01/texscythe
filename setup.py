8dcaaaffa8f7730b38fb57c2fff410e8c3807e0e
Edd Barrett <vext01@gmail.com>
2015-05-18 23:01:53 +0100

    Hook in tests to setup.py.

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 import sys
 
 from texscythe import VERSION
+
+# https://pytest.org/latest/goodpractises.html
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 setup(name            = 'texscythe',
     version           = VERSION,
@@ -24,5 +44,7 @@ setup(name            = 'texscythe',
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.7",
         "Topic :: Text Processing :: Markup :: LaTeX"
-    ]
+    ],
+    tests_require=['pytest'],
+    cmdclass = {'test': PyTest},
 )
