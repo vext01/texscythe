bdc14e8f05ef6ef883187c2c3cba60d6bf937e3a
Edd Barrett <vext01@gmail.com>
2016-03-19 17:04:29 +0000

    Add texlive 2015 tlpdb and add a test.

--- test/test_texlive.py
+++ test/test_texlive.py
@@ -6,9 +6,7 @@ from texscythe.orm import File, Package
 from texscythe import config
 
 
-class Test_TeXLive(AbstractTest):
-    """ Run some tests on a large texlive tlpdb """
-
+class Test_TeXLive2014(AbstractTest):
     def setup_method(self, method):
         self.cfg = config.Config(
                 plist=os.path.join(DIRPATH, "PLIST-texlive2014"),
@@ -16,10 +14,25 @@ class Test_TeXLive(AbstractTest):
                 dirs=False
                 )
 
-        super(Test_TeXLive, self).setup_method(method)
+        super(Test_TeXLive2014, self).setup_method(method)
 
     @pytest.mark.slow
-    def test_superficial(self):
-        # numbers determined independently by grep
+    def test_count0001(self):
         assert self.sess.query(Package).count() == 5987
         assert self.sess.query(File).count() == 139123
+
+
+class Test_TeXLive2015(AbstractTest):
+    def setup_method(self, method):
+        self.cfg = config.Config(
+                plist=os.path.join(DIRPATH, "PLIST-texlive2015"),
+                tlpdb=os.path.join(DIRPATH, "..", "texlive2015.tlpdb.gz"),
+                dirs=False
+                )
+
+        super(Test_TeXLive2015, self).setup_method(method)
+
+    @pytest.mark.slow
+    def test_count0001(self):
+        assert self.sess.query(Package).count() == 5995
+        assert self.sess.query(File).count() == 147335
